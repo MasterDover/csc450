9a3fa4addb47578e1bedfb7120a688ef801761da
masterdover <alexander.dover@cuw.edu>
2016-02-15 17:34:42 +0000

    first

---
+++ copy.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "tlpi_hdr.h"
+#ifndef BUF_SIZE
+#define BUF_SIZE 1024
+#endif
+/* Allow "cc -D" to override definition */
+int main(int argc, char *argv[])
+{
+  int inputFd, outputFd, openFlags; mode_t filePerms;
+  ssize_t numRead;
+  char buf[BUF_SIZE];
+  FILE *inputFp, *outputFp;
+  int total;
+
+
+  /* Open input and output files */
+  inputFd = open(argv[1], O_RDONLY);
+  openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+  filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; /* rw-rw-rw- */
+  outputFd = open(argv[2], openFlags, filePerms);
+
+
+
+if (inputFp = fopen(argv[1], "r"))    
+    {
+  outputFp = fopen(argv[2], "w+");
+  int i = 0;
+  char nextChar = getc(inputFp);
+  int numCharacters = 0;
+  while (nextChar != EOF) {
+    numCharacters++;
+    nextChar = getc(inputFp);
+}
+total = numCharacters;
+  fseek(inputFp, -1, SEEK_END);
+
+  for (int j = total; j >= 0; j--) {
+    char currChar = fgetc(inputFp);
+    fseek(inputFp, -2, SEEK_CUR);
+    fputc(currChar, outputFp);
+  }
+}
+  /* Transfer data until we encounter end of input or an error 
+  while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
+  {
+    
+    if (write(outputFd, buf, numRead) != numRead)
+    {
+      printf("ouch\n");
+    }
+    count--;
+  }
+  */
+  close(inputFd);
+  close(outputFd);
+  exit(EXIT_SUCCESS);
+}
+
+int count_characters(FILE *f) 
+{
+    fseek(f, -1L, 2);
+    int last_pos = ftell(f);
+    last_pos++;
+    return last_pos;
+}
